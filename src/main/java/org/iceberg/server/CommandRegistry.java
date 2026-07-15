b859f60c0d14fc8188ce241d139c9e726883f899
Guilherme Gules Moreira <guilhermegules@gmail.com>
2026-07-14 21:36:12 -0300

    fix: replace Map.of() with Map.ofEntries() to support 12 command registrations

--- src/main/java/org/iceberg/server/CommandRegistry.java
+++ src/main/java/org/iceberg/server/CommandRegistry.java
@@ -17,7 +17,6 @@ import org.iceberg.server.command.SetCommand;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Level;
@@ -29,20 +28,20 @@ public class CommandRegistry {
     private static final Logger LOG = Logger.getLogger(CommandRegistry.class.getName());
 
     public CommandRegistry(Store store, Path savePath) {
-        this.commands = new HashMap<>(Map.of(
-            "PING", new PingCommand(),
-            "ECHO", new EchoCommand(),
-            "SET", new SetCommand(store),
-            "GET", new GetCommand(store),
-            "EXISTS", new ExistsCommand(store),
-            "DEL", new DelCommand(store),
-            "INCR", new IncrCommand(store),
-            "DECR", new DecrCommand(store),
-            "LPUSH", new LpushCommand(store),
-            "RPUSH", new RpushCommand(store)
-        ));
-        this.commands.put("LRANGE", new LrangeCommand(store));
-        this.commands.put("SAVE", new SaveCommand(store, savePath));
+        this.commands = Map.ofEntries(
+            Map.entry("PING", new PingCommand()),
+            Map.entry("ECHO", new EchoCommand()),
+            Map.entry("SET", new SetCommand(store)),
+            Map.entry("GET", new GetCommand(store)),
+            Map.entry("EXISTS", new ExistsCommand(store)),
+            Map.entry("DEL", new DelCommand(store)),
+            Map.entry("INCR", new IncrCommand(store)),
+            Map.entry("DECR", new DecrCommand(store)),
+            Map.entry("LPUSH", new LpushCommand(store)),
+            Map.entry("RPUSH", new RpushCommand(store)),
+            Map.entry("LRANGE", new LrangeCommand(store)),
+            Map.entry("SAVE", new SaveCommand(store, savePath))
+        );
     }
 
     public RespValue execute(RespValue request) {
